369b912f450d8264103c85b877103fd96f9fff87
kafolatplast <asadnodirovichid@gmail.com>
2025-09-29 10:19:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+  const categories = {
+    triggers: [
+      { name: "Триггер 1", img: "images/trigger1.png", info: "Резьба 28/410 • Цвет: белый • Материал: PP" },
+      { name: "Триггер 2", img: "images/trigger2.png", info: "Резьба 28/410 • Цвет: черный • Материал: PP" },
+      { name: "Триггер 3", img: "images/trigger3.png", info: "Резьба 28/410 • Цвет: синий • Материал: PP" },
+      { name: "Триггер 4", img: "images/trigger4.png", info: "Резьба 28/410 • Цвет: красный • Материал: PP" },
+      { name: "Триггер 5", img: "images/trigger5.png", info: "Резьба 28/410 • Цвет: зелёный • Материал: PP" },
+      { name: "Триггер 6", img: "images/trigger6.png", info: "Резьба 28/410 • Цвет: прозрачный • Материал: PP" },
+      { name: "Триггер 7", img: "images/trigger7.png", info: "Резьба 28/410 • Цвет: жёлтый • Материал: PP" },
+      { name: "Триггер 8", img: "images/trigger8.png", info: "Резьба 28/410 • Цвет: серебристый • Материал: PP" },
+      { name: "Триггер 9", img: "images/trigger9.png", info: "Резьба 28/410 • Цвет: золотой • Материал: PP" },
+      { name: "Триггер 10", img: "images/trigger9.png", info: "Резьба 28/410 • Цвет: золотой • Материал: PP" },      
+      { name: "Триггер 11", img: "images/trigger10.png", info: "Резьба 28/410 • Цвет: матовый чёрный • Материал: PP" }
+    ],
+    dispensers: [
+      { name: "Дозатор 1", img: "images/dispenser1.png", info: "24/410 • Цвет: белый • Материал: PP" },
+      { name: "Дозатор 2", img: "images/dispenser2.png", info: "24/410 • Цвет: чёрный • Материал: PP" },
+      { name: "Дозатор 3", img: "images/dispenser3.png", info: "28/410 • Цвет: серебро • Материал: ABS" },
+      { name: "Дозатор 4", img: "images/dispenser4.png", info: "28/410 • Цвет: золото • Материал: ABS" },
+      { name: "Дозатор 5", img: "images/dispenser5.png", info: "33/410 • Цвет: прозрачный • Материал: PP" },
+      { name: "Дозатор 6", img: "images/dispenser6.png", info: "33/410 • Цвет: синий • Материал: PP" },
+      { name: "Дозатор 7", img: "images/dispenser7.png", info: "38/410 • Цвет: красный • Материал: PP" },
+      { name: "Дозатор 8", img: "images/dispenser8.png", info: "38/410 • Цвет: зелёный • Материал: PP" },
+      { name: "Дозатор 9", img: "images/dispenser9.png", info: "40/410 • Цвет: чёрный матовый • Материал: ABS" },
+      { name: "Дозатор 10", img: "images/dispenser10.png", info: "40/410 • Цвет: белый глянец • Материал: ABS" }
+    ],
+    sprayers: [
+      { name: "Спрейер 1", img: "images/sprayer1.png", info: "Трубка 160мм • Цвет: прозрачный" },
+      { name: "Спрейер 2", img: "images/sprayer2.png", info: "Трубка 180мм • Цвет: белый" },
+      { name: "Спрейер 3", img: "images/sprayer3.png", info: "Трубка 200мм • Цвет: чёрный" },
+      { name: "Спрейер 4", img: "images/sprayer4.png", info: "Трубка 220мм • Цвет: красный" },
+      { name: "Спрейер 5", img: "images/sprayer5.png", info: "Трубка 240мм • Цвет: синий" },
+      { name: "Спрейер 6", img: "images/sprayer6.png", info: "Трубка 260мм • Цвет: зелёный" },
+      { name: "Спрейер 7", img: "images/sprayer7.png", info: "Трубка 280мм • Цвет: серебро" },
+      { name: "Спрейер 8", img: "images/sprayer8.png", info: "Трубка 300мм • Цвет: золото" },
+      { name: "Спрейер 9", img: "images/sprayer9.png", info: "Трубка 320мм • Цвет: серый" },
+      { name: "Спрейер 10", img: "images/sprayer10.png", info: "Трубка 340мм • Цвет: чёрный матовый" }
+    ],
+    caps: [
+      { name: "Крышка 1", img: "images/cap1.png", info: "Флип-топ • 24/410" },
+      { name: "Крышка 2", img: "images/cap2.png", info: "Флип-топ • 28/410" },
+      { name: "Крышка 3", img: "images/cap3.png", info: "Винтовая • 24/410" },
+      { name: "Крышка 4", img: "images/cap4.png", info: "Винтовая • 28/410" },
+      { name: "Крышка 5", img: "images/cap5.png", info: "Флип-топ • 33/410" },
+      { name: "Крышка 6", img: "images/cap6.png", info: "Флип-топ • 38/410" },
+      { name: "Крышка 7", img: "images/cap7.png", info: "Винтовая • 33/410" },
+      { name: "Крышка 8", img: "images/cap8.png", info: "Винтовая • 38/410" },
+      { name: "Крышка 9", img: "images/cap9.png", info: "Флип-топ • 40/410" },
+      { name: "Крышка 10", img: "images/cap10.png", info: "Винтовая • 40/410" }
+    ],
+    pet: [
+      { name: "ПЭТ флакон 1", img: "images/pet1.png", info: "100 мл • Прозрачный" },
+      { name: "ПЭТ флакон 2", img: "images/pet2.png", info: "150 мл • Прозрачный" },
+      { name: "ПЭТ флакон 3", img: "images/pet3.png", info: "200 мл • Синий" },
+      { name: "ПЭТ флакон 4", img: "images/pet4.png", info: "250 мл • Коричневый" },
+      { name: "ПЭТ флакон 5", img: "images/pet5.png", info: "300 мл • Прозрачный" },
+      { name: "ПЭТ флакон 6", img: "images/pet6.png", info: "400 мл • Прозрачный" },
+      { name: "ПЭТ флакон 7", img: "images/pet7.png", info: "500 мл • Синий" },
+      { name: "ПЭТ флакон 8", img: "images/pet8.png", info: "750 мл • Коричневый" },
+      { name: "ПЭТ флакон 9", img: "images/pet9.png", info: "1000 мл • Прозрачный" },
+      { name: "ПЭТ флакон 10", img: "images/pet10.png", info: "1500 мл • Прозрачный" }
+    ],
+    pe: [
+      { name: "Полиэтиленовый флакон 1", img: "images/pe1.png", info: "50 мл • Белый" },
+      { name: "Полиэтиленовый флакон 2", img: "images/pe2.png", info: "100 мл • Белый" },
+      { name: "Полиэтиленовый флакон 3", img: "images/pe3.png", info: "150 мл • Белый" },
+      { name: "Полиэтиленовый флакон 4", img: "images/pe4.png", info: "200 мл • Белый" },
+      { name: "Полиэтиленовый флакон 5", img: "images/pe5.png", info: "250 мл • Белый" },
+      { name: "Полиэтиленовый флакон 6", img: "images/pe6.png", info: "300 мл • Белый" },
+      { name: "Полиэтиленовый флакон 7", img: "images/pe7.png", info: "400 мл • Белый" },
+      { name: "Полиэтиленовый флакон 8", img: "images/pe8.png", info: "500 мл • Белый" },
+      { name: "Полиэтиленовый флакон 9", img: "images/pe9.png", info: "750 мл • Белый" },
+      { name: "Полиэтиленовый флакон 10", img: "images/pe10.png", info: "1000 мл • Белый" }
+    ],
+    pp: [
+      { name: "Полипропиленовый флакон 1", img: "images/pp1.png", info: "75 мл • Матовый" },
+      { name: "Полипропиленовый флакон 2", img: "images/pp2.png", info: "150 мл • Матовый" },
+      { name: "Полипропиленовый флакон 3", img: "images/pp3.png", info: "225 мл • Матовый" },
+      { name: "Полипропиленовый флакон 4", img: "images/pp4.png", info: "300 мл • Матовый" },
+      { name: "Полипропиленовый флакон 5", img: "images/pp5.png", info: "375 мл • Матовый" },
+      { name: "Полипропиленовый флакон 6", img: "images/pp6.png", info: "450 мл • Матовый" },
+      { name: "Полипропиленовый флакон 7", img: "images/pp7.png", info: "525 мл • Матовый" },
+      { name: "Полипропиленовый флакон 8", img: "images/pp8.png", info: "600 мл • Матовый" },
+      { name: "Полипропиленовый флакон 9", img: "images/pp9.png", info: "675 мл • Матовый" },
+      { name: "Полипропиленовый флакон 10", img: "images/pp10.png", info: "750 мл • Матовый" }
+    ]
+  };
+
+ const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const modalTitle = document.getElementById("modal-title");
+  const modalInfo = document.getElementById("modal-info");
+  const closeBtn = document.querySelector(".close");
+
+  function showCategory(category) {
+    const container = document.getElementById("catalog");
+    container.innerHTML = "";
+
+    let items = [];
+    if (category === "all") {
+      Object.values(categories).forEach(arr => items.push(...arr));
+    } else {
+      items = categories[category] || [];
+    }
+
+    items.forEach(item => {
+      const card = document.createElement("div");
+      card.className = "product-card";
+      card.innerHTML = `
+        <img src="${item.img}" alt="${item.name}">
+        <h3>${item.name}</h3>
+        <p>${item.info}</p>
+      `;
+      card.onclick = () => {
+        modal.style.display = "flex";
+        modalImg.src = item.img;
+        modalTitle.textContent = item.name;
+        modalInfo.textContent = item.info;
+      };
+      container.appendChild(card);
+    });
+  }
+
+  closeBtn.onclick = () => modal.style.display = "none";
+  window.onclick = (e) => { if (e.target === modal) modal.style.display = "none"; }
+
+  // По умолчанию показываем все
+  showCategory("all");
+
